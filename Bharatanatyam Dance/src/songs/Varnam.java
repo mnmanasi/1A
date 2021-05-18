ca41198e7770f20aaf98cd3f8973845fe57afad4
mnmanasi <manasi.mnm@gmail.com>
2021-05-18 08:50:01 -0500

    Getters & Setters & Constructor
    Created constructor & getters and setters for all instance variables

--- Bharatanatyam Dance/src/songs/Varnam.java
+++ Bharatanatyam Dance/src/songs/Varnam.java
@@ -1,10 +1,67 @@
 package songs;
 
 public class Varnam {
+	
 	private String name;
 	private String ragam;
 	private String talam;
 	private int minutes;
+	
+	public Varnam(String title, String raga, String tala, int length) {
+		name = title;
+		ragam = raga;
+		talam = tala;
+		minutes = length;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the ragam
+	 */
+	public String getRagam() {
+		return ragam;
+	}
+	/**
+	 * @param ragam the ragam to set
+	 */
+	public void setRagam(String ragam) {
+		this.ragam = ragam;
+	}
+	/**
+	 * @return the talam
+	 */
+	public String getTalam() {
+		return talam;
+	}
+	/**
+	 * @param talam the talam to set
+	 */
+	public void setTalam(String talam) {
+		this.talam = talam;
+	}
+	/**
+	 * @return the minutes
+	 */
+	public int getMinutes() {
+		return minutes;
+	}
+	/**
+	 * @param minutes the minutes to set
+	 */
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
 	
 	
 
